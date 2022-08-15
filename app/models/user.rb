d8d5e6887d3a3927b921d05c29c65c7ac4b0305a
adpoppo <nisiwow8@gmail.com>
2022-08-15 21:48:11 +0900

    usersコントローラーの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true, length: { maximum: 10 }
+  validates :password,
+  format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は半角英数字混合6文字以上で入力してください' }
 end
